8a4a5b4e4358d1ae115be62566f82b49a6db5a17
RafaelCabec <rafael.marquardt@hotmail.com>
2022-02-28 11:26:42 -0300

    youtube

---
+++ src/Views/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text , View} from "react-native";
+import Stream from "./stream";
+
+export default function Home() {
+    return <>
+     <Text>home!</Text>
+     <Stream />    
+    </>
+    
+}
---
+++ src/Views/stream.js
@@ -0,0 +1,40 @@
+import React, { useCallback, useState } from "react";
+import { Button, View , Alert } from "react-native";
+import YoutubePlayer from "react-native-youtube-iframe";
+
+export default function Stream() {
+    const [ playing, setPlayin] = useState(false);
+
+    const onStateChange = useCallback ((state) => {
+        if (state === "ended") {
+            setPlayin(false);
+            Alert.alert("A transmissão acabou!");
+        }
+    }, []);
+
+    const togglePlaying = useCallback (() => {
+        setPlayin((prev) => !prev);
+    }, []);
+
+    const VideoId = "Edk0TfK94pA";
+
+    return (
+        <View>
+            <YoutubePlayer
+                height = {500}
+                width = {450} 
+                play = {playing}
+                videoId = {VideoId}
+                onChangeState = {onStateChange}
+            />
+            {/* 
+            
+            <Button 
+                title = { playing ? "pause" : "play" }
+                onPress = {togglePlaying}
+            /> 
+            
+            */}
+        </View>
+    )
+}
